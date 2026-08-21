3f1a098a367d871ad97639cbb6993f90b7e68751
geoxinho <georgex.edg@gmail.com>
2026-08-21 09:38:35 +0100

    style: remove unnecessary gap from Help Desk link in Footer component

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -63,7 +63,7 @@ export default function Footer() {
               <li>
                 <Link
                   href="/help"
-                  className="hover:text-[#111111] font-medium transition-colors flex items-center gap-1.5"
+                  className="hover:text-[#111111] font-medium transition-colors flex items-center"
                 >
                   Help Desk{" "}
                 </Link>{" "}
